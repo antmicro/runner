6f9a8fd353c4402f0583be727660c939b72b0cc9
eric sciple <ericsciple@users.noreply.github.com>
2021-04-30 15:48:53 -0500

    minor cleanup in composite (#1045)

--- src/Runner.Worker/Handlers/ScriptHandler.cs
+++ src/Runner.Worker/Handlers/ScriptHandler.cs
@@ -32,7 +32,7 @@ namespace GitHub.Runner.Worker.Handlers
             // We don't want to display the internal workings if composite (similar/equivalent information can be found in debug)
             void writeDetails(string message)
             {
-                if (ExecutionContext.InsideComposite)
+                if (ExecutionContext.IsEmbedded)
                 {
                     ExecutionContext.Debug(message);
                 }
@@ -58,7 +58,7 @@ namespace GitHub.Runner.Worker.Handlers
                     firstLine = firstLine.Substring(0, firstNewLine);
                 }
 
-                writeDetails(ExecutionContext.InsideComposite ? $"Run {firstLine}" : $"##[group]Run {firstLine}");
+                writeDetails(ExecutionContext.IsEmbedded ? $"Run {firstLine}" : $"##[group]Run {firstLine}");
             }
             else
             {
@@ -144,7 +144,7 @@ namespace GitHub.Runner.Worker.Handlers
                 }
             }
 
-            writeDetails(ExecutionContext.InsideComposite ? "" : "##[endgroup]");
+            writeDetails(ExecutionContext.IsEmbedded ? "" : "##[endgroup]");
         }
 
         public async Task RunAsync(ActionRunStage stage)
