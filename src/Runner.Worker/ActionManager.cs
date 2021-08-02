c97999510fe80d52a7599a0d0e32ba00cd07d706
Thomas Boop <52323235+thboop@users.noreply.github.com>
2021-08-02 14:57:25 -0400

    Composite Code Cleanup (#1232)
    * composite polish
    
    * Cleanup Condition Handling
    
    * Refactor ConditionTraceWriter
    
    * pr feedback
    
    * cleanup

--- src/Runner.Worker/ActionManager.cs
+++ src/Runner.Worker/ActionManager.cs
@@ -248,7 +248,10 @@ namespace GitHub.Runner.Worker
                                 {
                                     _cachedEmbeddedPreSteps[parentStepId] = new List<Pipelines.ActionStep>();
                                 }
-                                _cachedEmbeddedPreSteps[parentStepId].Add(action);
+                                // Clone action so we can modify the condition without affecting the original
+                                var clonedAction = action.Clone() as Pipelines.ActionStep;
+                                clonedAction.Condition = definition.Data.Execution.InitCondition;
+                                _cachedEmbeddedPreSteps[parentStepId].Add(clonedAction);
                             }
                         }
 
@@ -259,7 +262,10 @@ namespace GitHub.Runner.Worker
                                 // If we haven't done so already, add the parent to the post steps
                                 _cachedEmbeddedPostSteps[parentStepId] = new Stack<Pipelines.ActionStep>();
                             }
-                            _cachedEmbeddedPostSteps[parentStepId].Push(action);
+                            // Clone action so we can modify the condition without affecting the original
+                            var clonedAction = action.Clone() as Pipelines.ActionStep;
+                            clonedAction.Condition = definition.Data.Execution.CleanupCondition;
+                            _cachedEmbeddedPostSteps[parentStepId].Push(clonedAction);
                         }
                     }
                 }
