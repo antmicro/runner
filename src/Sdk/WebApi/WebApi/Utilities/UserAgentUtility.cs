6a10df4dc5d3c55476ffffd89115ca1be7c69cb0
Adam Olech <aolech@antmicro.com>
2021-09-20 13:59:10 +0000

    UserAgentUtility: don't extract version string

--- src/Sdk/WebApi/WebApi/Utilities/UserAgentUtility.cs
+++ src/Sdk/WebApi/WebApi/Utilities/UserAgentUtility.cs
@@ -21,21 +21,7 @@ namespace GitHub.Services.WebApi.Utilities.Internal
 
         private static List<ProductInfoHeaderValue> ConstructDefaultRestUserAgent()
         {
-            // Pick up the assembly version from this dll
             String fileVersion = "unavailable";
-            try
-            {
-                AssemblyFileVersionAttribute attr = typeof(UserAgentUtility).GetTypeInfo().Assembly.GetCustomAttribute<AssemblyFileVersionAttribute>();
-                if (attr != null)
-                {
-                    fileVersion = attr.Version;
-                }
-            }
-            catch (Exception e)
-            {
-                Trace.WriteLine("DefaultUserAgent: Unable to get fileVersion: " + e.ToString());
-            }
-
             String commentValue = string.Format("(NetStandard; {0})", RuntimeInformation.OSDescription.Replace('(', '[').Replace(')', ']').Trim());
 
             return new List<ProductInfoHeaderValue> {
