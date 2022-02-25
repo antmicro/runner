9acbc14b67a0712343ad88d39de02909c37b165c
Rob Herley <robherley@github.com>
2022-02-24 21:01:49 -0500

    Update summary max file size annotation error text (#1712)
    * add doc link to summary file size err annotation
    
    * Update src/Runner.Common/Constants.cs
    
    Co-authored-by: Konrad Pabjan <konradpabjan@github.com>
    
    * remove i18n from url
    
    Co-authored-by: Konrad Pabjan <konradpabjan@github.com>

--- src/Runner.Common/Constants.cs
+++ src/Runner.Common/Constants.cs
@@ -173,6 +173,7 @@ namespace GitHub.Runner.Common
             public static readonly string WorkerCrash = "WORKER_CRASH";
             public static readonly string UnsupportedCommand = "UNSUPPORTED_COMMAND";
             public static readonly string UnsupportedCommandMessageDisabled = "The `{0}` command is disabled. Please upgrade to using Environment Files or opt into unsecure command execution by setting the `ACTIONS_ALLOW_UNSECURE_COMMANDS` environment variable to `true`. For more information see: https://github.blog/changelog/2020-10-01-github-actions-deprecating-set-env-and-add-path-commands/";
+            public static readonly string UnsupportedSummarySize = "$GITHUB_STEP_SUMMARY upload aborted, supports content up to a size of {0}k, got {1}k. For more information see: https://docs.github.com/actions/using-workflows/workflow-commands-for-github-actions#adding-a-markdown-summary";
         }
 
         public static class RunnerEvent
