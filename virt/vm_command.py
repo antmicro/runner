4bcd729e60ca4cc70bc2558f91e67b711fd60355
Adam Olech <aolech@antmicro.com>
2022-04-29 15:57:36 +0000

    vm_command: start utility container

--- virt/vm_command.py
+++ virt/vm_command.py
@@ -336,9 +336,15 @@ def create_vm(instance_number, container_file, disk_name=None, preemptible_overr
         sys.exit(1)
 
     container_sif_location = '/mnt/container.sif'
+
     node_sif_location = '/opt/sif/node.sif'
     node_zip_src = 'https://github.com/antmicro/github-actions-singularity-node/releases/download/node-16-v0.1.3/node-16-v0.1.3.zip'
     node_zip_dst = '/mnt/node.zip'
+
+    util_sif_location = '/opt/sif/util.sif'
+    util_zip_src = 'https://github.com/antmicro/github-actions-singularity-utility-container/releases/download/v0.1.0/gha-utility-container-v0.1.0.zip'
+    util_zip_dst = '/mnt/util.zip'
+
     infer_dns_cmd = "$(echo $SSH_CONNECTION | awk '{ print $1 }')"
 
     # Truncate local rsyslog log file
@@ -380,8 +386,10 @@ def create_vm(instance_number, container_file, disk_name=None, preemptible_overr
             'echo "------------------"',
             'echo "::endgroup::"',
             f'sudo sh -c "test ! -f {node_sif_location} && curl -o {node_zip_dst} -Ls {node_zip_src} && unzip -p {node_zip_dst} node-16-alpine3.14.sif > {node_sif_location}"',
+            f'sudo sh -c "test ! -f {util_sif_location} && curl -o {util_zip_dst} -Ls {util_zip_src} && unzip -p {util_zip_dst} image.sif > {util_sif_location}"',
             f'sudo singularity pull --nohttps {container_sif_location} docker://{infer_dns_cmd}:5000/{container_file}',
             f'sudo singularity instance start -C -e --dns {infer_dns_cmd} --overlay /mnt/3 --bind /mnt/2:/root,/mnt/aux {node_sif_location} node',
+            f'sudo singularity instance start -C -e --dns {infer_dns_cmd} --writable-tmpfs {util_sif_location} util',
             f'echo "::group::Starting {container_file}..."',
             f'sudo singularity --debug instance start -C -e --dns {infer_dns_cmd} --overlay /mnt/1 --bind /mnt/2:/root,/mnt/aux {container_sif_location} i',
             'echo "::endgroup::"',
