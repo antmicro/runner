bbdd4244dfa381f68bdc23817dfb44a4ac46b5d3
Luke Tomlinson <luketomlinson@github.com>
2023-03-24 14:00:34 -0400

    Listen directly to Broker for messages (#2500)

--- src/Runner.Common/ConfigurationStore.cs
+++ src/Runner.Common/ConfigurationStore.cs
@@ -52,6 +52,9 @@ namespace GitHub.Runner.Common
         [DataMember(EmitDefaultValue = false)]
         public bool UseV2Flow { get; set; }
 
+        [DataMember(EmitDefaultValue = false)]
+        public string ServerUrlV2 { get; set; }
+
         [IgnoreDataMember]
         public bool IsHostedServer
         {
