0da5d06330f785bead44c6ccdce0980b4e761393
Yashwanth Anantharaju <yaananth@users.noreply.github.com>
2023-04-27 13:59:12 -0400

    handle conflict errors from run service (#2570)
    * handle conflict errors from run service
    
    * nit: formatting
    
    * fix formatting

--- src/Runner.Listener/Runner.cs
+++ src/Runner.Listener/Runner.cs
@@ -454,7 +454,17 @@ namespace GitHub.Runner.Listener
                                     {
                                         var runServer = HostContext.CreateService<IRunServer>();
                                         await runServer.ConnectAsync(new Uri(messageRef.RunServiceUrl), creds);
-                                        jobRequestMessage = await runServer.GetJobMessageAsync(messageRef.RunnerRequestId, _messageQueueLoopTokenSource.Token);
+                                        try
+                                        {
+                                            jobRequestMessage =
+                                            await runServer.GetJobMessageAsync(messageRef.RunnerRequestId,
+                                            _messageQueueLoopTokenSource.Token);
+                                        }
+                                        catch (TaskOrchestrationJobAlreadyAcquiredException)
+                                        {
+                                            Trace.Info("Job is already acquired, skip this message.");
+                                            continue;
+                                        }
                                     }
 
                                     _jobDispatcher.Run(jobRequestMessage, runOnce);
