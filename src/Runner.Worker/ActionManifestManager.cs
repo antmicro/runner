04df17f6c179a42ada4953b23a58a163af920580
Adam Olech <aolech@antmicro.com>
2024-04-22 16:07:59 +0200

    ActionManifestManager: accept node20 in the 'using' param

--- src/Runner.Worker/ActionManifestManager.cs
+++ src/Runner.Worker/ActionManifestManager.cs
@@ -452,7 +452,8 @@ namespace GitHub.Runner.Worker
                     }
                 }
                 else if (string.Equals(usingToken.Value, "node12", StringComparison.OrdinalIgnoreCase) ||
-                         string.Equals(usingToken.Value, "node16", StringComparison.OrdinalIgnoreCase))
+                         string.Equals(usingToken.Value, "node16", StringComparison.OrdinalIgnoreCase) ||
+                         string.Equals(usingToken.Value, "node20", StringComparison.OrdinalIgnoreCase))
                 {
                     if (string.IsNullOrEmpty(mainToken?.Value))
                     {
@@ -491,7 +492,7 @@ namespace GitHub.Runner.Worker
                 }
                 else
                 {
-                    throw new ArgumentOutOfRangeException($"'using: {usingToken.Value}' is not supported, use 'docker', 'node12' or 'node16' instead.");
+                    throw new ArgumentOutOfRangeException($"'using: {usingToken.Value}' is not supported.");
                 }
             }
             else if (pluginToken != null)
